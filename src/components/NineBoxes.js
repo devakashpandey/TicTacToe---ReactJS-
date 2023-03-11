7b5a0fe79b2254f865c95031e5bbb40ba1e7ed4f
DevAkky <developerakky@gmaiil.com>
2023-03-12 00:47:20 +0530

    responsiveness

--- src/components/NineBoxes.js
+++ src/components/NineBoxes.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import "./NineBoxes.css"
-import Box from './Box';
+import React from "react";
+import "./NineBoxes.css";
+import Box from "./Box";
 
-function NineBoxes({nineBox , onClick}) {
+function NineBoxes({ nineBox, onClick }) {
   return (
-   
- <>
- <div className='boxes'>
-
- {
-   nineBox.map((currEle, idx)=>{
-       return <Box value={currEle} onClick={()=> currEle === null && onClick(idx)} /> // MAP TO LOOPING THE BOXES 9 TIMES
-   })
- }
-</div> 
-
-
- </>
-
-  )
+    <>
+      <div className="boxes">
+        {nineBox.map((currEle, idx) => {
+          return (
+            <Box
+              value={currEle}
+              onClick={() => currEle === null && onClick(idx)}
+            />
+          ); // MAP TO LOOPING THE BOXES 9 TIMES
+        })}
+      </div>
+    </>
+  );
 }
 
 export default NineBoxes;
